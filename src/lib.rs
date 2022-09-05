b83aad01b57d40fc1f664a0de83899ca9ddf6fe3
Christopher Biscardi <chris@christopherbiscardi.com>
2022-09-05 04:17:37 -0700

    refactor textures and bind groups

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
+mod texture;
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
@@ -58,6 +60,7 @@ struct State {
     index_buffer: wgpu::Buffer, 
     num_indices: u32,
     diffuse_bind_group: wgpu::BindGroup,
+    diffuse_texture: texture::Texture,
 }
 
 impl State {
@@ -116,66 +119,8 @@ impl State {
             
             
             let diffuse_bytes = include_bytes!("happy-tree.png");
-            let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-            let diffuse_rgba = diffuse_image.to_rgba8();
-    
-            use image::GenericImageView;
-            let dimensions = diffuse_image.dimensions();
-     
-            let texture_size = wgpu::Extent3d {
-                width: dimensions.0,
-                height: dimensions.1,
-                depth_or_array_layers: 1,
-            };
-            let diffuse_texture = device.create_texture(
-                &wgpu::TextureDescriptor {
-                    // All textures are stored as 3D, we represent our 2D texture
-                    // by setting depth to 1.
-                    size: texture_size,
-                    mip_level_count: 1, // We'll talk about this a little later
-                    sample_count: 1,
-                    dimension: wgpu::TextureDimension::D2,
-                    // Most images are stored using sRGB so we need to reflect that here.
-                    format: wgpu::TextureFormat::Rgba8UnormSrgb,
-                    // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
-                    // COPY_DST means that we want to copy data to this texture
-                    usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-                    label: Some("diffuse_texture"),
-                }
-            );
-    
-            queue.write_texture(
-                // Tells wgpu where to copy the pixel data
-                wgpu::ImageCopyTexture {
-                    texture: &diffuse_texture,
-                    mip_level: 0,
-                    origin: wgpu::Origin3d::ZERO,
-                    aspect: wgpu::TextureAspect::All,
-                },
-                // The actual pixel data
-                &diffuse_rgba,
-                // The layout of the texture
-                wgpu::ImageDataLayout {
-                    offset: 0,
-                    bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
-                    rows_per_image: std::num::NonZeroU32::new(dimensions.1),
-                },
-                texture_size,
-            );
-            
-            // We don't need to configure the texture view much, so let's
-            // let wgpu define it.
-            let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
-            let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-                address_mode_u: wgpu::AddressMode::ClampToEdge,
-                address_mode_v: wgpu::AddressMode::ClampToEdge,
-                address_mode_w: wgpu::AddressMode::ClampToEdge,
-                mag_filter: wgpu::FilterMode::Linear,
-                min_filter: wgpu::FilterMode::Nearest,
-                mipmap_filter: wgpu::FilterMode::Nearest,
-                ..Default::default()
-            });
-    
+            let diffuse_texture = texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "happy-tree.png").unwrap(); // CHANGED!
+
             let texture_bind_group_layout =
                 device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
                     entries: &[
@@ -207,11 +152,11 @@ impl State {
                     entries: &[
                         wgpu::BindGroupEntry {
                             binding: 0,
-                            resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+                            resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
                         },
                         wgpu::BindGroupEntry {
                             binding: 1,
-                            resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+                            resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler),
                         }
                     ],
                     label: Some("diffuse_bind_group"),
@@ -295,6 +240,7 @@ impl State {
             index_buffer,
             num_indices,
             diffuse_bind_group,
+            diffuse_texture,
         }
     }
 
